9a7afa8c438d2692c34c22771afda75eee7b9f5f
Shagun-29 <shagun.sharma@venturepact.com>
2018-04-24 16:35:27 +0530

    --

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { PrivacyComponent } from './shared/components/privacy/privacy.component';
 import { TermsComponent } from './shared/components/terms/terms.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { CoreModule } from './core.module';
 
 
 @NgModule({
@@ -22,7 +21,6 @@ import { CoreModule } from './core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule,
     RouterModule.forRoot(routes)  ],
   providers: [],
   bootstrap: [AppComponent]
