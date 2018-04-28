da8d9130952a310ba46e2a9b57fcd5ac6b087a77
chandulella <chandulella77@gmail.com>
2018-04-28 09:59:12 +0530

    routing

--- src/app/site/landing-pages/survey/survey.component.ts
+++ src/app/site/landing-pages/survey/survey.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.css']
+  styleUrls: ['./survey.component.css','../../../../assets/css/allPage_minified.css']
 })
 export class SurveyComponent implements OnInit {
 
--- src/app/site/site.routing.ts
+++ src/app/site/site.routing.ts
@@ -1,12 +1,24 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizComponent } from './landing-pages/quiz/quiz.component';
+import { SurveyComponent } from './landing-pages/survey/survey.component';
+import { CalculatorComponent } from './landing-pages/calculator/calculator.component';
+import { IncreaseSalesComponent } from './landing-pages/increase-sales/increase-sales.component';
+import { IncreaseRoiComponent } from './landing-pages/increase-roi/increase-roi.component';
+import { IncreaseConversionsComponent } from './landing-pages/increase-conversions/increase-conversions.component';
 
 export const routes: Routes = [
+    { path: '', loadChildren: './home/home.module#HomeModule' },
+    { path: 'calculator', component: CalculatorComponent },
     { path: 'examples', loadChildren: './examples/examples.module#ExamplesModule' },
     { path: 'features', loadChildren: './features/features.module#FeaturesModule' },
-    { path: '', loadChildren: './home/home.module#HomeModule' },
+    { path: 'increase-sales', component: IncreaseSalesComponent },
+    { path: 'increase-roi', component: IncreaseRoiComponent },
+    { path: 'increase-conversions', component: IncreaseConversionsComponent },
     { path: 'pricing', loadChildren: './pricing/pricing.module#PricingModule' },
-    { path: 'why-interactive', loadChildren: './why-interactive/why-interactive.module#WhyInteractiveModule' }
+    { path: 'quiz', component: QuizComponent },
+    { path: 'survey', component: SurveyComponent },
+    { path: 'why-interactive', loadChildren: './why-interactive/why-interactive.module#WhyInteractiveModule' },
 
 ];
 
