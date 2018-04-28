9ea19ee7507cc26a0220364bb111c32bf1bb71b3
chandulella <chandulella77@gmail.com>
2018-04-28 09:45:31 +0530

    routing

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import {VideoUrlService} from '../shared/services/video-url.service'
 import { RouterModule } from '@angular/router';
 import { routes } from './site.routing';
+import { CalculatorComponent } from './landing-pages/calculator/calculator.component';
+import { IncreaseConversionsComponent } from './landing-pages/increase-conversions/increase-conversions.component';
+import { IncreaseRoiComponent } from './landing-pages/increase-roi/increase-roi.component';
+import { IncreaseSalesComponent } from './landing-pages/increase-sales/increase-sales.component';
+import { QuizComponent } from './landing-pages/quiz/quiz.component';
+import { SurveyComponent } from './landing-pages/survey/survey.component';
 
 @NgModule({
   imports: [
@@ -12,6 +18,6 @@ import { routes } from './site.routing';
   providers:[
     VideoUrlService
   ],
-  declarations: []
+  declarations: [CalculatorComponent,IncreaseConversionsComponent,IncreaseRoiComponent,IncreaseSalesComponent,QuizComponent,SurveyComponent]
 })
 export class SiteModule { }
