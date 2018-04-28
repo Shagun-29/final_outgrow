9fd39928ccbf2543912c15024e563a8ff3c3b135
Shagun-29 <shagun.sharma@venturepact.com>
2018-04-28 09:48:31 +0530

    --

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import {VideoUrlService} from '../shared/services/video-url.service'
 import { RouterModule } from '@angular/router';
 import { routes } from './site.routing';
+import { CalculatorComponent } from './landing-pages/calculator/calculator.component';
+import { SurveyComponent } from './landing-pages/survey/survey.component';
+import { QuizComponent } from './landing-pages/quiz/quiz.component';
+import { IncreaseSalesComponent } from './landing-pages/increase-sales/increase-sales.component';
+import { IncreaseRoiComponent } from './landing-pages/increase-roi/increase-roi.component';
+import { IncreaseConversionsComponent } from './landing-pages/increase-conversions/increase-conversions.component';
 
 @NgModule({
   imports: [
@@ -12,6 +18,6 @@ import { routes } from './site.routing';
   providers:[
     VideoUrlService
   ],
-  declarations: []
+  declarations: [CalculatorComponent, SurveyComponent, QuizComponent, IncreaseSalesComponent, IncreaseRoiComponent, IncreaseConversionsComponent]
 })
 export class SiteModule { }
