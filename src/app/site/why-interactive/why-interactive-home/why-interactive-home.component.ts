ef161ccf90316e0251e26d05c48b807ab593fdbc
Shagun-29 <shagun.sharma@venturepact.com>
2018-06-29 16:58:12 +0530

    --

--- src/app/site/why-interactive/why-interactive-home/why-interactive-home.component.ts
+++ src/app/site/why-interactive/why-interactive-home/why-interactive-home.component.ts
@@ -2,19 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import * as $ from 'jquery';
+declare let Swiper: any;
 
 @Component({
-  selector: 'app-why-interactive-home',
-  templateUrl: './why-interactive-home.component.html',
-  styleUrls: ['./why-interactive-home.component.css','../../../../assets/css/allPage_minified.css','../../../../assets/css/custom-sa.css','../../../../assets/css/sahil-hover.css','../../../../assets/css/home-responsive.css','../../../../assets/css/why-responsive.css']
+ selector: 'app-why-interactive-home',
+ templateUrl: './why-interactive-home.component.html',
+ styleUrls: ['./why-interactive-home.component.css']
 })
 export class WhyInteractiveHomeComponent implements OnInit {
 
-  constructor(router:Router, title:Title) {
-    title.setTitle("Why Interactive Content ? | Outgrow");
-   }
+ constructor(router:Router, title:Title) {
+ title.setTitle("Why Interactive Content ? | Outgrow");
+ }
 
-  ngOnInit() {
-  }
+ ngOnInit() {
+ $(document).ready(function(){
+
+ 
+ var swipera = new Swiper('.swiper-container-first', {
+ pagination: '.swiper-pagination',
+ paginationClickable: true,
+ nextButton: '.swiper-button-next',
+ prevButton: '.swiper-button-prev',
+ spaceBetween: 30,
+ slidesPerView: 1,
+ centeredSlides: true,
+ autoplayDisableOnInteraction: false
+ });
+});
+ }
 
 }
