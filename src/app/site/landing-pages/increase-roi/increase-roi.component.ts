6a3603f9847a8ebeaf9cf4c097ca4e8573021faa
Shagun-29 <shagun.sharma@venturepact.com>
2018-07-13 12:33:56 +0530

    -footer-

--- src/app/site/landing-pages/increase-roi/increase-roi.component.ts
+++ src/app/site/landing-pages/increase-roi/increase-roi.component.ts
@@ -101,6 +101,8 @@ export class IncreaseRoiComponent implements OnInit {
   ngOnInit() {
     let header = document.querySelector('.navbar-fixed-top');
     header.classList.add('show');
+    let footer = document.querySelector('.footer-14');
+    footer.classList.add('show');
     this.loader.classList.add('hide');
     var gridJS = document.createElement('script')
     gridJS.src = 'https://repuso.com/widgets/grid.js';
--- src/app/site/landing-pages/increase-sales/increase-sales.component.ts
+++ src/app/site/landing-pages/increase-sales/increase-sales.component.ts
@@ -97,6 +97,8 @@ export class IncreaseSalesComponent implements OnInit {
   ngOnInit() {
     let header = document.querySelector('.navbar-fixed-top');
     header.classList.add('show');
+    let footer = document.querySelector('.footer-14');
+    footer.classList.add('show');
     this.loader.classList.add('hide');
     this.shuffleCalcs('Auto');
     var gridJS = document.createElement('script')
