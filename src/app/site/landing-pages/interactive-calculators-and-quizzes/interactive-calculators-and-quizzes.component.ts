63e2c6a0682012d2c670baa9e7c475d7424be5ff
Shagun-29 <shagun.sharma@venturepact.com>
2018-07-17 13:14:03 +0530

    new component added

---
+++ src/app/site/landing-pages/interactive-calculators-and-quizzes/interactive-calculators-and-quizzes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interactive-calculators-and-quizzes',
+  templateUrl: './interactive-calculators-and-quizzes.component.html',
+  styleUrls: ['./interactive-calculators-and-quizzes.component.css']
+})
+export class InteractiveCalculatorsAndQuizzesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
