21752a5234a613d3caadae8ccbb5b63e584ec7b1
Shagun-29 <shagun.sharma@venturepact.com>
2018-07-20 15:33:12 +0530

    --changes--

--- src/app/site/landing-pages/interactive-calculators-and-quizzes/interactive-calculators-and-quizzes.component.ts
+++ src/app/site/landing-pages/interactive-calculators-and-quizzes/interactive-calculators-and-quizzes.component.ts
@@ -12,8 +12,42 @@ export class InteractiveCalculatorsAndQuizzesComponent implements OnInit {
   loader = document.querySelector('.preloader');
   constructor() { }
 
+  
+  changeHeightRes () {
+    window.location.replace('#bfrVid')
+    document.getElementsByClassName("video-img-inner-res")[0].innerHTML = "<div class='embed-responsive embed-responsive-16by9'><iframe class='outgrow-video' src='https://www.youtube.com/embed/PmN_MY5kNrE?vq=hd720&amp;rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&amp;iv_load_policy=3' frameborder='0' allowfullscreen></iframe></div>";
+    var xDiv = document.getElementById('video-main-rs');
+    if (xDiv.style.height == '')
+      xDiv.style.height = '460px';
+    else
+      xDiv.style.height = '';
+}
+
+changeHeightWeb1 () {
+  window.location.replace('#bfrVid')
+  document.getElementById("him1").innerHTML = "<div class='embed-responsive embed-responsive-16by9'><iframe class='outgrow-video' src='https://www.youtube.com/embed/PmN_MY5kNrE?vq=hd720&amp;rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&amp;iv_load_policy=3' frameborder='0' allowfullscreen></iframe></div>";
+  var xDiv = document.getElementById('video-main');
+
+  if (xDiv.style.height == '')
+    xDiv.style.height = '574px';
+  else
+    xDiv.style.height = '';
+}
+
+changeHeightWebXL () {
+  
+          window.location.replace('#bfrVid')
+          document.getElementById("him1-xl").innerHTML = "<div class='embed-responsive embed-responsive-16by9'><iframe class='outgrow-video' src='https://www.youtube.com/embed/PmN_MY5kNrE?vq=hd720&amp;rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&amp;iv_load_policy=3' frameborder='0' allowfullscreen></iframe></div>";
+          var xDiv = document.getElementById('video-main-xl');
+
+          if (xDiv.style.height == '')
+            xDiv.style.height = '1160px';
+          else
+            xDiv.style.height = '';
+        }
   ngOnInit() {
     this.loader.classList.add('hide');
   }
 
+ 
 }
