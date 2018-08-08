45522b6de1cb6acdfcbaa59b1c96b2833fcab223
Shagun-29 <shagun.sharma@venturepact.com>
2018-08-08 11:47:12 +0530

    ltd-1 created

---
+++ src/app/site/landing-pages/ltd-1/ltd-1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ltd-1',
+  templateUrl: './ltd-1.component.html',
+  styleUrls: ['./ltd-1.component.css']
+})
+export class Ltd1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
