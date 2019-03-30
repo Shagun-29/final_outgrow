58e173d4858242547980a914b5750071ab58e64f
Shagun-29 <shagun.sharma@venturepact.com>
2019-03-30 17:24:10 +0530

    --adding header and footer--

--- src/app/site/use-cases/outgrow-hr/outgrow-hr.component.ts
+++ src/app/site/use-cases/outgrow-hr/outgrow-hr.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'app-outgrow-hr',
@@ -7,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutgrowHrComponent implements OnInit {
   loader = document.querySelector('.preloader');
-
-  constructor() { }
-
+  constructor(title:Title) { 
+   title.setTitle('Improve Marketing and Sales ROI | Outgrow');
+  }
   ngOnInit() {
+    let header = document.querySelector('.navbar-fixed-top');
+    header.classList.add('show');
+    let footer = document.querySelector('.footer-14');
+    footer.classList.add('show');
     this.loader.classList.add('hide');
+
 
   }
 
